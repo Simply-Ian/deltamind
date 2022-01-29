c05fed8f14116b9099df542a59e3ffb68c42a928
Falcko-dev <iwan.cars@yandex.ru>
2022-01-29 22:32:07 +0500

    Адаптация сервера под heroku: конфигурационные файлы, креды db и порт подключения берутся из переменных окружения. При запуске на ненастроенной среде: должны быть настроены переменные окружения PORT и DATABASE_URL.

--- DatabaseDriver/Models.py
+++ DatabaseDriver/Models.py
@@ -1,23 +1,19 @@
 import peewee_asyncext
 from peewee import Model, CharField, IntegerField, TextField, DateField, ForeignKeyField
 from playhouse.postgres_ext import ArrayField
-from playhouse.db_url import connect
+from playhouse.db_url import parse
 import os
 
 
-# postgres://tgkcvtovpaodej:b9d0ff843daca5dd9104661b6360bff34962287776f5b4221806f96d5f1afa12@ec2-54-78-36-245.eu-west-1.compute.amazonaws.com:5432/d6vmbi96rjrm3
-db_url = os.environ.get('DATABASE_URL').split(':')
-db_url[0] = 'postgresext'
-db_url = ':'.join(db_url)
+db_url = parse(os.environ.get('DATABASE_URL'))
 print(db_url)
-database = connect(db_url)
 # postgresql://postgres:my_password@localhost:5432/
 # postgres://postgres:26082005@127.0.0.1:5432/delta
-# database = peewee_asyncext.PostgresqlExtDatabase(database=db_url['database'],
-# 						user=db_url['user'],
-# 						password=db_url['password'],
-# 						host=db_url['host'],
-# 						port=db_url['port'])
+database = peewee_asyncext.PostgresqlExtDatabase(database=db_url['database'],
+						user=db_url['user'],
+						password=db_url['password'],
+						host=db_url['host'],
+						port=db_url['port'])
 
 
 class BaseModel(Model):
